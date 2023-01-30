3229185c2ee5599df0d6c3237ae5f3fdc0c20a9a
Lucky Patel <113097395+lucky7xx@users.noreply.github.com>
2023-01-30 11:16:55 +0530

    resposnsive

--- src/components/ui_components/Contact.js
+++ src/components/ui_components/Contact.js
@@ -7,23 +7,48 @@ import { socialLinks } from "./../dynamic_data/Details";
 
 const Contact = () => {
   return (
+    // <div id="contact">
+    //   <Container>
+    //     <Row>
+    //       <p
+    //         className="subtitle mt-5 text-center"
+    //         data-aos="flip-down"
+    //         data-aos-duration="1000"
+    //         data-aos-mirror="true"
+    //       >
+    //         We can get in touch here!
+    //       </p>
+    //       <Col
+    //         className="d-flex justify-content-center pt-5 mt-5 pb-5 mb-5"
+    //         data-aos="flip-up"
+    //         data-aos-duration="1000"
+    //         data-aos-mirror="true"
+    //       >
+    //         {socialLinks.map((link) => (
+    //           <Links key={link.id} href={link.url}>
+    //             <link.icon sx={{ fontSize: "50px", color: "black" }} />
+    //           </Links>
+    //         ))}
+    //       </Col>
+    //     </Row>
+    //   </Container>
+    // </div>
     <div id="contact">
       <Container>
-        <Row>
-          <p
-            className="subtitle mt-5 text-center"
-            data-aos="flip-down"
-            data-aos-duration="1000"
-            data-aos-mirror="true"
-          >
-            We can get in touch here!
-          </p>
-          <Col
-            className="d-flex justify-content-center pt-5 mt-5 pb-5 mb-5"
-            data-aos="flip-up"
-            data-aos-duration="1000"
-            data-aos-mirror="true"
-          >
+        <Row className="text-center">
+          <Col xs={12}>
+            <p
+              className="subtitle mt-5"
+              data-aos="flip-down"
+              data-aos-duration="1000"
+              data-aos-mirror="true"
+            >
+              We can get in touch here!
+            </p>
+          </Col>
+        </Row>
+        <Row className="pt-5 mt-5 pb-5 mb-5">
+          <Col xs={12} className="d-flex justify-content-center">
             {socialLinks.map((link) => (
               <Links key={link.id} href={link.url}>
                 <link.icon sx={{ fontSize: "50px", color: "black" }} />
--- src/components/ui_components/Skills.js
+++ src/components/ui_components/Skills.js
@@ -6,21 +6,51 @@ import Container from "react-bootstrap/Container";
 import { skillsData } from "../dynamic_data/Details";
 const Skills = () => {
   return (
+    // <div id="skills">
+    //   <Container className="h-100">
+    //     <Row className="mt-5 pt-5 mb-5 pb-5 ">
+    //       <Col>
+    //         <p
+    //           className="subtitle mt-5 text-center"
+    //           data-aos="flip-down"
+    //           data-aos-duration="1000"
+    //           data-aos-mirror="true"
+    //         >
+    //           What I Know!
+    //         </p>
+
+    //         <div
+    //           className="col d-flex justify-content-center mt-5 mb-5 pt-5 pb-5"
+    //           data-aos="zoom-in-up"
+    //           data-aos-duration="1000"
+    //           data-aos-mirror="true"
+    //         >
+    //           {skillsData.map((skill) => (
+    //             <ImageComponent key={skill.id} languageIcon={skill.image} />
+    //           ))}
+    //         </div>
+    //       </Col>
+    //     </Row>
+    //   </Container>
+    // </div>
     <div id="skills">
       <Container className="h-100">
-        <Row className="mt-5 pt-5 mb-5 pb-5 ">
-          <Col>
+        <Row className="mt-5 pt-5 mb-5 pb-5">
+          <Col xs={12} className="text-center">
             <p
-              className="subtitle mt-5 text-center"
+              className="subtitle mt-5"
               data-aos="flip-down"
               data-aos-duration="1000"
               data-aos-mirror="true"
             >
               What I Know!
             </p>
-
+          </Col>
+        </Row>
+        <Row className="mt-5 mb-5">
+          <Col xs={12}>
             <div
-              className="col d-flex justify-content-center mt-5 mb-5 pt-5 pb-5"
+              className="d-flex flex-wrap justify-content-center"
               data-aos="zoom-in-up"
               data-aos-duration="1000"
               data-aos-mirror="true"
